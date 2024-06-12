ad129e24b35ff0dd097c7df524e7cbb8376e5eb1
Yessica Miguel <yessicam.dev@gmail.com>
2024-06-12 14:26:38 -0600

    Initial Commit

---
+++ CasoPractivoEKT/CasoPractivoEKTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasoPractivoEKTApp.swift
+//  CasoPractivoEKT
+//
+//  Created by Yess development on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CasoPractivoEKTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
